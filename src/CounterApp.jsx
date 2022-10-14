32848eadd98825588c17f371c6127efcfee3167b
MattoDev <mateofonseca.profesional@gmail.com>
2022-10-13 20:56:27 -0500

    Hooks

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
-const handleAdd = (event) => {
-  console.log(event);
-  // console.log(newValue);
-};
 export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+
+  const handleAdd = (event) => {
+    console.log(event);
+    // console.log(newValue);
+    // setCounter(counter + 1);
+    setCounter((c) => c + 1);
+  };
+  const handleSubtract = (event) => {
+    console.log(event);
+    // console.log(newValue);
+    // setCounter(counter + 1);
+    setCounter((c) => c - 1);
+  };
+  const handleReset = (event) => {
+    console.log(event);
+    // console.log(newValue);
+    // setCounter(counter + 1);
+    setCounter((c) => (c = value));
+  };
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> {value} </h2>
-      <button onClick={(event) => handleAdd(event, "Hola")}>+1</button>
+      <h2> {counter} </h2>
+      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleSubtract}>-1</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 };
